2ec667f13fb57e28dedf25fc25e08ae746e2fb5c
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-07-08 16:43:07 +0200

    quizz bientôt terminé

--- main.php
+++ main.php
@@ -1,6 +1,4 @@
 <?php
-
-
 $questions = [
     "Quelle est la couleur du cheval blanc d'Henri IV?\n1.Blanc\n2.Rouge\n3.Noir\n",
     "Date de la prise de la Bastille ?\n1.1750\n2.1789\n3.1800\n",
@@ -10,93 +8,21 @@ $questions = [
 ];
 
 
-$reponses = {1, 2, 3, 1, 3};
+$reponses = [1, 2, 3, 1, 3];
 $score = 0;
+$scorefinal= $score + 10;
 
 $NB_QUESTIONS = count($questions);
 
-for($i = 0; $i < $NB_QUESTIONS; $i++){
-    echo $questions[$i]."\n";
-    // Votre code
-    
+for ($i = 0; $i < $NB_QUESTIONS; $i++) {
+    echo $questions[$i] . "\n";
+    echo $questions[$i];
+    $ce_que_utilisateur_repond = trim(fgets(STDIN)); // le programme ATTEND que l'utilisateur tape au clavier (lecture du fichier stdin)
+    if ($questions[$i] == $reponses[$i]) {
+        echo "you win\n";
+    } else {
+        echo "tu ne gagnes pas question suivante \n";
+    }
 }
 
-echo $score." \n";
-
-
-// Synopsis
-// Vous devez concevoir un jeu de type quiz. Le programme pose des questions à l'utilisateur. Si l'utilisateur répond correctement il gagne des points. Une fois que toutes les questions sont passées le programme dit si l'utilisateur à gagner ou non (le joueur gagne si il à répondu juste à plus de 50% des questions).
-
-// En langage PHP.
-
-// Interface du jeu
-// Voici un exemple de partie :
-
-// ###################################################
-// ######## Qui veux gagner des millions ?! ##########
-// ###################################################
-
-// ###################################################
-// Score : 0
-// ###################################################
-
-// Question 1 :
-
-// Qu'elle est la couleur du chaval blanc d'Henri IV ?
-// 1. Noir 
-// 2. Blanc
-// 3. Rouge
-
-// 2
-
-// Suspennnnce ! 
-
-// Bien joué !
-
-// *Le score augemente de 10*
-
-// ###################################################
-// Score : 10
-// ###################################################
-
-// Question 2 :
-
-// Prise de la Bastille ?
-// 1. 1789
-// 2. 1750
-// 3. 1800
-
-// 3
-
-// Suspennnnce ! 
-
-// NON ! 
-
-// *Le score n'augemente pas. :(*
-
-// ###################################################
-// Score : 10
-// ###################################################
-
-// Question [X] :
-
-// [Autre question ?
-// 1. bla
-// 2. blo]
-
-// [user_input]
-
-// Suspennnnce ! 
-
-// [Réponse]
-
-// [indiquer le score]
-
-// ###################################################
-// Score : [score_du_joueur]
-// ###################################################
-
-// ########### GAME OVER ###########
-// Pourcentage de bonne réponse  : 70%
-
-// Bien joué tu as gagné des millions !
+echo $score . " \n";
