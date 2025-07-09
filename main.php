f5d590f54295f02a32286ee0b85c34c1e2d99d2d
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-07-09 10:20:20 +0200

    quizz fini

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 $questions = [
-    "Quelle est la couleur du cheval blanc d'Henri IV?\n1.Blanc\n2.Rouge\n3.Noir\n",
+    "Quelle est la couleur du cheval blanc d'Henri IV?2\n1.Blanc\n2.Rouge\n3.Noir\n",
     "Date de la prise de la Bastille ?\n1.1750\n2.1789\n3.1800\n",
     "Quel est le plus grand océan du monde ?\n1.Océan Atlantique\n2.Océan Indien\n3.Océan Pacifique\n",
     "Qui a écrit Les Misérables ?\n1.Victor Hugo\n2.Emile Zola\n3.Marcel Proust\n",
@@ -10,19 +10,38 @@ $questions = [
 
 $reponses = [1, 2, 3, 1, 3];
 $score = 0;
-$scorefinal= $score + 10;
 
 $NB_QUESTIONS = count($questions);
 
 for ($i = 0; $i < $NB_QUESTIONS; $i++) {
     echo $questions[$i] . "\n";
-    echo $questions[$i];
-    $ce_que_utilisateur_repond = trim(fgets(STDIN)); // le programme ATTEND que l'utilisateur tape au clavier (lecture du fichier stdin)
-    if ($questions[$i] == $reponses[$i]) {
-        echo "you win\n";
+    $ce_que_utilisateur_repond = (int)trim(fgets(STDIN)); // le programme ATTEND que l'utilisateur tape au clavier (lecture du fichier stdin)
+
+
+    if ($ce_que_utilisateur_repond === $reponses[$i]) {
+        echo "Bonne réponse + 10 points\n";
+        $score += 10;
     } else {
-        echo "tu ne gagnes pas question suivante \n";
+        echo "Mauvaise réponse, question suivante \n";
     }
 }
 
-echo $score . " \n";
+echo "Ton score est de " . $score . " points \n";
+
+
+// il faut parler si user a un score au moins 50% il a gagné sinon il a perdu
+
+
+
+// $calcul = (15/50) * 100
+
+$calcul = ($score / 50) * 100;
+
+
+if ($calcul <= 25) {
+
+    echo "l'utilisateur a perdu \n";
+} else {
+
+    echo "l'utilisateur a gagné\n";
+}
